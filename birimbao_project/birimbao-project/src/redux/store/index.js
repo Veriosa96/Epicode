dfecf252ce86fcb0d7d351b9968a3970051e451e
Saverio Caracappa <saveriocaracappa@gmail.com>
2023-05-08 13:50:50 +0200

    start project

---
+++ birimbao_project/birimbao-project/src/redux/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+//import { persistStore, persistReducer } from "redux-persist";
+
+const store = configureStore({
+    //reduce
+})
+
+export default store
