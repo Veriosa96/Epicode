59cda749d04cd51e3e094a8d07e4a2464817bb8f
Saverio Caracappa <saveriocaracappa@gmail.com>
2023-05-09 10:22:42 +0200

    start project

--- birimbao_project/birimbao-project/src/App.js
+++ birimbao_project/birimbao-project/src/App.js
@@ -1,12 +1,20 @@
-// import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+//import sideBar from './components/sideBar/sideBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <BrowserRouter>
+      <loginPage/>
+      <Routes>
+      <navBar/>
+        <Route path="/" element={<homePage/>}/>
+        <Route path="/" element={<sideBar/>}/>
+        <Route path="/" element={<footer/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
